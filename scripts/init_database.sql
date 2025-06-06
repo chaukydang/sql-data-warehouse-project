eadc6b41ece915cc3b0c16b3d04e5c472e542d38
Đặng Châu Kỳ <94186949+chaukydang@users.noreply.github.com>
2025-06-06 16:30:33 +0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
